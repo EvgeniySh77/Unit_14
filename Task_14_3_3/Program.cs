d34223f3a3ac100f458f78611f7e3cee07b9a548
evgen <evgen@FESKO>
2023-02-01 23:39:12 +0300

    Добавьте файлы проекта.

---
+++ Task_14_3_3/Program.cs
@@ -0,0 +1,59 @@
+﻿using Task_14_3_3.Models;
+
+namespace Task_14_3_3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //  создаём пустой список с типом данных Contact
+            var phoneBook = new List<Contact>();
+
+            // добавляем контакты
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            //сортируем контакты телефонной книги сперва по имени, а затем по фамилии.
+            var sortPhoneBook = phoneBook.OrderBy(x => x.Name)
+                                         .ThenBy(x => x.LastName);
+
+            //Выводим список контактов телефонной книги отсортированый по критериям задания 
+            foreach (var сontact in sortPhoneBook)
+                Console.WriteLine(сontact.Name + " " + сontact.LastName + ": " + сontact.PhoneNumber);
+            Console.WriteLine();
+
+            while (true)
+            {
+                // Читаем введенный с консоли символ
+                var input = Console.ReadKey().KeyChar;
+
+                // проверяем, число ли это
+                var parsed = Int32.TryParse(input.ToString(), out int pageNumber);
+
+                // если не соответствует критериям - показываем ошибку
+                if (!parsed || pageNumber < 1 || pageNumber > 3)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Страницы не существует");
+                }
+                // если соответствует - запускаем вывод
+                else
+                {
+                    // пропускаем нужное количество элементов и берем 2 для показа на странице
+                    var pageContent = phoneBook.Skip((pageNumber - 1) * 2).Take(2);
+                    Console.WriteLine();
+
+                    // выводим результат
+                    foreach (var entry in pageContent)
+                        Console.WriteLine(entry.Name + " " + entry.LastName + ": " + entry.PhoneNumber);
+
+                    Console.WriteLine();
+                }
+            }
+        }   
+    }
+}
